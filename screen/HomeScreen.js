e83a36fd57bb0c311c1f6f23ed4c159f18db03fc
Dixon-Afreh <dixonafreh20@gmail.com>
2024-07-11 17:12:59 +0000

    new

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,180 +1,7 @@
-// import { MaterialCommunityIcons, FontAwesome5 } from "@expo/vector-icons";
-// import React, { useState } from "react";
-// import { View, StyleSheet, ScrollView } from "react-native";
-// import {
-//   Appbar,
-//   Card,
-//   Title,
-//   Button,
-
-//   Text,
-// } from "react-native-paper";
-// import Colors from "../config/Colors";
-// import FontSize from "../config/FontSize";
-// import { useRoute } from "@react-navigation/native";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
-
-// const HomeScreen = ({ navigation }) => {
-//   const route = useRoute();
-//   const [child, setchild] = useState();
-//   // const { data } = route.params
-//   // const {age ,grade,name}= data
-
-//   // const userName = data.name;
-//   const getchild = async () => {
-//     try {
-//       const Childdata = await AsyncStorage.getItem("child");
-//       const childJSON = JSON.parse(Childdata);
-//       // console.log(childJSON.name);
-//       setchild(childJSON);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   getchild();
-//   return (
-//     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-//       <View style={styles.container}>
-//         {/* Top Bar */}
-//         <Appbar.Header
-//           style={{ justifyContent: "space-between", flexDirection: "row" }}
-//         >
-//           <Appbar.Content
-//             title={`Welcome ${child?.name}`}
-//             style={styles.appbarContent}
-//           />
-//           {/* <MaterialCommunityIcons name="brain" size={30} color={Colors.background} /> */}
-//           <MaterialCommunityIcons
-//             name="bell"
-//             size={30}
-//             color={Colors.background}
-//           />
-//           {/* </View> */}
-//         </Appbar.Header>
-
-//         <Card style={styles.card}>
-//           <Card.Content>
-//             <Title>General Cognitive Test</Title>
-//             <View style={styles.cardTextContainer2}>
-//               <Title style={styles.cardText}></Title>
-//             </View>
-//             <Button
-//               mode="contained"
-//               onPress={() => navigation.navigate("TestOneInitial")}
-//               style={{ backgroundColor: Colors.background }}
-//             >
-//               Start Test
-//             </Button>
-//           </Card.Content>
-//         </Card>
-//         <Card style={styles.card}>
-//           <Card.Content>
-//             <Title>General Handwriting Test</Title>
-//             <View style={styles.cardTextContainer2}>
-//               <Title style={styles.cardText}></Title>
-//             </View>
-//             <Button
-//               mode="contained"
-//               onPress={() => navigation.navigate("HandwritingInitial")}
-//               // onPress={() => navigation.navigate("ResultTwo")}
-//               style={{ backgroundColor: Colors.background }}
-//             >
-//               Upload Image
-//             </Button>
-//           </Card.Content>
-//         </Card>
-//         <Card style={styles.card}>
-//           <Card.Content>
-//             <Title>General Reading Test</Title>
-//             <View style={styles.cardTextContainer2}>
-//               <Title style={styles.cardText}></Title>
-//             </View>
-//             <Button
-//               mode="contained"
-//               onPress={() => navigation.navigate("ReadingInitial")}
-//               style={{ backgroundColor: Colors.background }}
-//             >
-//               Upload Video
-//             </Button>
-//           </Card.Content>
-//         </Card>
-//         <Card style={styles.cardd}>
-//           <Card.Content style={{ justifyContent: "center" }}>
-//             <View style={{ flexDirection: "row", gap: 10 }}>
-//               <MaterialCommunityIcons
-//                 name="alphabetical-variant"
-//                 size={40}
-//                 color={Colors.white}
-//               />
-//               <MaterialCommunityIcons
-//                 name="brain"
-//                 size={40}
-//                 color={Colors.white}
-//               />
-//             </View>
-//             <Text style={{ color: Colors.white, fontSize: FontSize.large }}>
-//               Daily Crosswordd
-//             </Text>
-//           </Card.Content>
-//         </Card>
-//       </View>
-//     </ScrollView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//   },
-//   progressContainer: {
-//     paddingHorizontal: 20,
-//     paddingTop: 20,
-//   },
-//   cardTextContainer2: {
-//     alignItems: "center",
-//   },
-//   progressBar: {
-//     height: 10,
-//   },
-//   graphContainer: {
-//     height: 200,
-//     marginHorizontal: 16,
-//     marginVertical: 20,
-//     width: "80%",
-//   },
-//   graph: {
-//     flex: 1,
-//   },
-//   card: {
-//     margin: 20,
-//   },
-//   cardd: {
-//     margin: 20,
-//     backgroundColor: Colors.background,
-//     height: 200,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-//   cardTextContainer: {
-//     marginVertical: 10,
-//   },
-//   cardText: {
-//     fontSize: 20,
-//     fontWeight: 600,
-//     lineHeight: 24,
-//   },
-//   appbarContent: {
-//     alignItems: "center",
-//     justifyContent: "space-around",
-//   },
-// });
-
-// export default HomeScreen;
-
 import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRoute } from "@react-navigation/native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import {
   StyleSheet,
@@ -202,16 +29,11 @@ const HomeScreen = ({ navigation }) => {
   useEffect(() => {
     setImageUrl(getRandomImageUrl());
   }, []);
-  // const Predictions = await AsyncStorage.setItem('predictio')
-
-  // const {newmessage2,newMessage} = route.params
 
   const getPrediction = async () => {
     try {
       const AppPredictionOne = await AsyncStorage.getItem("prediction");
-      // const childJSON = JSON.parse(Childdata);
-      // console.log(childJSON.name);
-      // setchild(childJSON);
+
       console.log(AppPredictionOne);
     } catch (error) {
       console.log(error);
@@ -245,32 +67,41 @@ const HomeScreen = ({ navigation }) => {
   return (
     <ScrollView style={styles.container}>
       <View style={styles.header}>
-        <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
-          <MaterialCommunityIcons name="menu" size={30} color={"white"} />
-          <MaterialCommunityIcons name="bell" size={30} color={"white"} />
-        </View>
+        {/* <MaterialCommunityIcons name="menu" size={30} color={"white"} /> */}
+
+        {/* <MaterialCommunityIcons name="settings" size={30} color={"white"} /> */}
         {/* <Button title="get" onPress={getPrediction} /> */}
         {/* <Button title="get" onPress={getPredictionTwo} /> */}
+        <View>
+          {/* <Image source={imageUrl} style={{ width: "200" }} /> */}
+        </View>
 
-        <Image source={imageUrl} style={{ width: "200" }} />
-
-        <View style={{ alignSelf: "center" }}>
+        <View
+          style={{
+            alignItems: "center",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
           {/* <Image
             style={styles.profilePic}
             source={{ uri: "https://via.placeholder.com/150" }} // Replace with actual image URI
           /> */}
-          <Image
-            style={styles.profilePic}
-            source={{ uri: imageUrl }} // Replace with actual image URI
+          <Image style={styles.profilePic} source={{ uri: imageUrl }} />
+          <Ionicons
+            name="settings"
+            size={30}
+            color={"white"}
+            onPress={() => navigation.navigate("Settings")}
           />
         </View>
         <Text style={styles.greeting}>
-          {`Welcome ${child?.name}`}{" "}
-          <MaterialCommunityIcons name="star" size={25} color={"yellow"} />
+          {`Welcome ${child?.name}`}
+          <MaterialCommunityIcons name="star" size={23} color={"#efdc4f"} />
         </Text>
         <Text style={styles.subGreeting}>
           Up your reading skill with{" "}
-          <Text style={{ color: "yellow" }}>LexyAfriq.</Text>
+          <Text style={{ color: "#efdc4f" }}>LexyAfriq.</Text>
         </Text>
       </View>
       <View style={styles.categoriesContainer}>
@@ -356,12 +187,12 @@ const styles = StyleSheet.create({
   },
   greeting: {
     color: "#fff",
-    fontSize: 24,
+    fontSize: 22,
     fontWeight: "bold",
   },
   subGreeting: {
     color: "#fff",
-    fontSize: 16,
+    fontSize: 13,
   },
   categoriesContainer: {
     padding: 20,
--- screen/Settings.js
+++ screen/Settings.js
@@ -24,13 +24,16 @@ import Colors from "../config/Colors";
 import FontSize from "../config/FontSize";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AuthContext } from "../Context";
+import { Icon } from "react-native-elements";
 
 const SettingsScreen = () => {
   const navigation = useNavigation();
+  const [child, setchild] = useState();
   const [sound, setSound] = useState();
   const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(true);
   const [isSoundEnabled, setIsSoundEnabled] = useState(false);
   const [visible, setVisible] = useState(false);
+  const [imageUrl, setImageUrl] = useState(getRandomImageUrl());
 
   const { setIsLoggedIn } = useContext(AuthContext);
 
@@ -74,6 +77,25 @@ const SettingsScreen = () => {
     setIsLoading(false);
   };
 
+  const getchild = async () => {
+    try {
+      const Childdata = await AsyncStorage.getItem("child");
+      const childJSON = JSON.parse(Childdata);
+      // console.log(childJSON.name);
+      setchild(childJSON);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getchild();
+
+   function getRandomImageUrl(width = 200, height = 200) {
+     return `https://picsum.photos/${width}/${height}?random=${Math.random()}`;
+   }
+   useEffect(() => {
+     setImageUrl(getRandomImageUrl());
+   }, []);
+
   return (
     <PaperProvider>
       <View style={styles.container}>
@@ -81,17 +103,19 @@ const SettingsScreen = () => {
       <Appbar.Content title="Settings" />
       </Appbar.Header> */}
         <ScrollView contentContainerStyle={styles.scrollViewContent}>
-          <View style={styles.avatarContainer}>
-            <Image
+          {/* <View style={styles.avatarContainer}> */}
+          {/* <Image
               source={require("../assets/ddd.jpg")}
               style={styles.avatar}
-            />
-            <View style={styles.infoContainer}>
-              <Text style={styles.name}>Dixon Afreh</Text>
+            /> */}
+          {/* <View style={styles.infoContainer}>
+              <Text style={styles.name}>{`${child?.name}`}</Text>
+              <Text style={styles.name}>{`Age : ${child?.age} years`}</Text>
+              <Text style={styles.name}>{`Grade: ${child?.grade}`}</Text>
               <Text style={styles.grade}>Level 100</Text>
-            </View>
+            </View> */}
 
-            <TouchableOpacity
+          {/* <TouchableOpacity
               style={styles.addButton}
               onPress={() => navigation.navigate("RegisterChild")}
             >
@@ -100,103 +124,95 @@ const SettingsScreen = () => {
                 size={30}
                 color={Colors.white}
               />
+            </TouchableOpacity> */}
+          {/* </View> */}
+          {/* <View style={styles.header}>
+            <Icon name="arrow-back" size={24} onPress={() => {}} />
+            <Text style={styles.headerTitle}>Settings</Text>
+          </View> */}
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Account</Text>
+            <TouchableOpacity style={styles.accountItem}>
+              {/* <Image
+                style={styles.profilePic}
+                source={{ uri: "https://via.placeholder.com/50" }} // Replace with actual image URI
+              /> */}
+              <Image style={styles.profilePic} source={{ uri: imageUrl }} />
+              <View style={styles.accountTextContainer}>
+                <Text style={styles.accountName}>{`${child?.name}`}</Text>
+                <Text
+                  style={styles.accountSubText}
+                >{`Age : ${child?.age} years`}</Text>
+                <Text
+                  style={styles.accountSubText}
+                >{`Grade: ${child?.grade}`}</Text>
+              </View>
+              {/* <Icon name="addition" size={24} /> */}
+              <MaterialCommunityIcons
+                name="plus"
+                size={28}
+                color={Colors.background}
+                onPress={() => navigation.navigate("RegisterChild")}
+                style={{ width: "bold" }}
+              />
             </TouchableOpacity>
           </View>
-          <List.Section>
-            <List.Subheader>Preferences</List.Subheader>
-            <Divider />
-            <Divider />
-            <View style={styles.settingItem}>
-            {/* <MaterialCommunityIcons name="notification" size={30} /> */}
-              <Text style={styles.settingText}>Enable Notifications</Text>
-              <Switch
-                value={isNotificationsEnabled}
-                onValueChange={() =>
-                  setIsNotificationsEnabled(!isNotificationsEnabled)
-                }
-              />
-            </View>
-            <Divider />
-            <Divider />
-            
-            <View style={styles.settingItem}>
-              <Text style={styles.settingText}>Play Background Sound</Text>
+
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Settings</Text>
+            <TouchableOpacity style={styles.settingsItem}>
+              <Icon name="settings" size={24} color="#f39c12" />
+              <Text style={styles.settingsText}>General</Text>
+              <Icon name="chevron-right" size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
+              <Icon name="language" size={24} color="#3498db" />
+              <Text style={styles.settingsText}>Language</Text>
+              <Text style={styles.settingsSubText}>English</Text>
+              <Icon name="chevron-right" size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
+              <Icon name="notifications" size={24} color="#9b59b6" />
+              <Text style={styles.settingsText}>Notifications</Text>
+              <Icon name="chevron-right" size={24} />
+            </TouchableOpacity>
+            <View style={styles.settingsItem}>
+              <Icon name="brightness-2" size={24} color="#34495e" />
+              <Text style={styles.settingsText}>Background Sound</Text>
+
               <Switch
                 value={isSoundEnabled}
                 onValueChange={() => setIsSoundEnabled(!isSoundEnabled)}
               />
             </View>
-            <Divider />
-            <Divider />
-            {/* <Divider />
-            <Divider /> */}
-
-            <List.Subheader style={styles.settingItem}>
-              App Information
-            </List.Subheader>
-            <Divider />
-            <Divider />
-            {/* <Divider />
-            <Divider /> */}
-            <List.Item
-              style={styles.settingItem}
-              title="About the App"
-              left={() => <List.Icon icon="information" />}
-            />
-            {/* <Divider />
-            <Divider /> */}
-            {/* <List.Item
-              style={styles.settingItem}
-              title="Terms of Service"
-              left={() => <List.Icon icon="file-document" />}
-            /> */}
-            <Divider />
-            <Divider />
-            <List.Item
-              style={styles.settingItem}
-              title="Privacy Policy"
-              left={() => <List.Icon icon="shield-lock" />}
-            />
-            <Divider />
-            <Divider />
-            {/* <Divider />
-            <Divider /> */}
-
-            <List.Subheader>Support</List.Subheader>
-            <Divider />
-            <Divider />
-            {/* <Divider />
-            <Divider /> */}
-            <List.Item
-              style={styles.settingItem}
-              title="Contact Support"
-              left={() => <List.Icon icon="help-circle" />}
-            />
-            <Divider />
-
-            <List.Item
-              title="Logout"
-              style={styles.settingItem}
-              left={() => <List.Icon icon="comment-question" />}
-              onPress={showDialog}
-            />
-            <Portal>
-              <Dialog visible={visible} onDismiss={hideDialog}>
-                <Dialog.Title>Logout</Dialog.Title>
-                <Dialog.Content>
-                  <Text>Do you want to logout your account? </Text>
-                </Dialog.Content>
-                <Dialog.Actions>
-                  <Button onPress={hideDialog}>Cancle</Button>
-                  <Button onPress={handleLogout}>Logout</Button>
-                </Dialog.Actions>
-              </Dialog>
-            </Portal>
-
-            <Divider />
-
-            {/* </List.Section> */}
-          </List.Section>
+            <TouchableOpacity style={styles.settingsItem}>
+              <Icon name="volume-up" size={24} color="#1abc9c" />
+              <Text style={styles.settingsText}>Sounds & Haptics</Text>
+              <Icon name="chevron-right" size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem}>
+              <Icon name="lock" size={24} color="#e74c3c" />
+              <Text style={styles.settingsText}>Security</Text>
+              <Icon name="chevron-right" size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.settingsItem} onPress={showDialog}>
+              <Icon name="exit-to-app" size={24} color="#95a5a6" />
+              <Text style={styles.settingsText}>Logout</Text>
+              <Icon name="chevron-right" size={24} />
+              <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                  <Dialog.Title>Logout</Dialog.Title>
+                  <Dialog.Content>
+                    <Text>Do you want to logout your account? </Text>
+                  </Dialog.Content>
+                  <Dialog.Actions>
+                    <Button onPress={hideDialog}>Cancle</Button>
+                    <Button onPress={handleLogout}>Logout</Button>
+                  </Dialog.Actions>
+                </Dialog>
+              </Portal>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
         {/* </ScrollView> */}
       </View>
@@ -207,52 +223,72 @@ const SettingsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.bluedemo,
+    backgroundColor: "white",
   },
-  avatarContainer: {
+  header: {
     flexDirection: "row",
     alignItems: "center",
     padding: 20,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
   },
-  scrollViewContent: {
-    paddingBottom: 20,
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 20,
   },
-  avatar: {
+  section: {
+    marginTop: 10,
+  },
+  sectionTitle: {
+    fontSize: 16,
+    color: "#666",
+    marginLeft: 20,
+    marginBottom: 10,
+  },
+  accountItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  profilePic: {
     width: 80,
     height: 80,
-    borderRadius: 40,
+    borderRadius: 50,
     marginRight: 20,
   },
-  infoContainer: {
+  accountTextContainer: {
     flex: 1,
-    justifyContent: "center",
   },
-  name: {
-    fontSize: FontSize.large,
+  accountName: {
+    fontSize: 16,
     fontWeight: "bold",
-    color: Colors.text,
   },
-  grade: {
-    fontSize: FontSize.medium,
-    color: Colors.text,
+  accountSubText: {
+    fontSize: 14,
+    color: "#666",
   },
-  addButton: {
-    backgroundColor: Colors.primary,
-    padding: 10,
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  settingItem: {
+  settingsItem: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    padding: 20,
+    backgroundColor: "#fff",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
   },
-  settingText: {
-    fontSize: FontSize.medium,
-    color: Colors.text,
+  settingsText: {
+    flex: 1,
+    fontSize: 16,
+    marginLeft: 20,
+  },
+  settingsSubText: {
+    fontSize: 14,
+    color: "#666",
+    marginRight: 10,
   },
 });
 
